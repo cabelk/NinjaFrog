6e3d204e38bd21ea10a249646e66a849e0b7034e
cabelk <cabelk@users.noreply.github.com>
2025-12-31 10:03:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -202,9 +202,7 @@ if (attackPad) {
     }
 
     create() {
-      this.cameras.main.setBackgroundColor("#0b0f14");
-
-      this.scale.resize(window.innerWidth, window.innerHeight);
+this.scale.resize(window.innerWidth, window.innerHeight);
       this.scale.on("resize", (gameSize) => {
         this.cameras.main.setViewport(0, 0, gameSize.width, gameSize.height);
         this.fitWorldToScreen(gameSize.width, gameSize.height);
@@ -535,10 +533,10 @@ flashRandomImage() {
   const config = {
     type: Phaser.AUTO,
     parent: "game",
+    transparent: true,
     width: window.innerWidth,
     height: window.innerHeight,
-    backgroundColor: "#0b0f14",
-    scene: [MainScene],
+scene: [MainScene],
     scale: { mode: Phaser.Scale.RESIZE, autoCenter: Phaser.Scale.CENTER_BOTH }
   };
 
